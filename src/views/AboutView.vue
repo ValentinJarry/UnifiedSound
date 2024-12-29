563929facf49b9bba02b5919165a7013f1ad9c3f
trinketmage <tranremi@live.fr>
2024-12-29 18:19:45 +0100

    feature(audio): interval selector

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount, type Ref } from 'vue';
+import { ref, computed, onMounted, onBeforeUnmount, type Ref } from 'vue';
 import { useAudio, ji5LimitModesFreqRatios, ji7LimitModesFreqRatios } from '@/composables/useAudio'
 
 const pristine = ref(true)
 const notes: Ref<string[]> = ref([])
+const currentMode: Ref<number> = ref(0)
+const currentInterval: Ref<string> = ref('NaturalMajor')
+
+const modes: Ref = ref([
+  {
+    label: "ji5LimitModesFreqRatios",
+    value: ji5LimitModesFreqRatios,
+  },
+  {
+    label: "ji7LimitModesFreqRatio",
+    value: ji7LimitModesFreqRatios
+  },
+])
+const intervals: Ref = ref([
+  'NaturalMajor',
+  'NaturalMinor'
+])
 
 const C4 = 261.63;
 
@@ -12,12 +29,11 @@ const { oscillator, gainNode, initialize } = useAudio({
 })
 
 const eventKeys = "QWERTYUI".split("").map(_ => `Key${_}`);
-
-const { NaturalMajor } = ji7LimitModesFreqRatios
 const handleKeyDown = ({ code }: { code: string }) => {
+  const interval = definedInterval.value
   const found = eventKeys.indexOf(code);
-  const toneKey = Object.keys(NaturalMajor)[found];
-  const factor = NaturalMajor[toneKey];
+  const toneKey = Object.keys(interval)[found];
+  const factor = interval[toneKey];
 
   if (found > -1) {
     clearKey(code)
@@ -44,6 +60,12 @@ const handleStart = () => {
   initialize()
 }
 
+const definedInterval = computed(() => {
+  const localMode = modes.value[currentMode.value].value
+  return localMode[currentInterval.value]
+
+})
+
 onMounted(() => {
   document.addEventListener("keydown", handleKeyDown)
   document.addEventListener("keyup", handleKeyUp)
@@ -60,9 +82,22 @@ onBeforeUnmount(() => {
 <template>
   <div class="about">
     <button @click="handleStart" v-if="pristine">start</button>
-    <div v-else class="keyboard">
-      <div :class="['key', { active: notes.indexOf(key) > -1 }]" v-for="key in eventKeys" :key="key">
-        {{ key }}
+    <div class="room" v-else>
+      <div class="controls">
+        <select v-model="currentMode">
+          <option v-for="({ label }, key) in modes" :value="key" :key="key">{{ label }}</option>
+        </select>
+        <select v-model="currentInterval">
+          <option v-for="(interval) in intervals" :key="interval">{{ interval }}</option>
+        </select>
+      </div>
+      <div class="keyboard">
+        <div :class="['key', { active: notes.indexOf(key) > -1 }]" v-for="(key, i) in eventKeys" :key="key">
+          <span>{{ Object.keys(definedInterval)[i] }}</span>
+          <span>{{ +definedInterval[Object.keys(definedInterval)[i]].toFixed(5)
+            }}</span>
+          <span>{{ +(definedInterval[Object.keys(definedInterval)[i]] * C4).toFixed(5) }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -81,6 +116,17 @@ button {
   cursor: pointer;
 }
 
+.room {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.controls {
+  display: flex;
+  gap: 10px;
+}
+
 .keyboard {
   display: flex;
   gap: 10px;
@@ -94,6 +140,7 @@ button {
   align-items: center;
   justify-content: center;
   color: rgba(0, 0, 0, 0.25);
+  flex-direction: column;
 }
 
 .key.active {
