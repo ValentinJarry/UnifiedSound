522311f2842171924e1c05e1f7a528fdc92edc31
trinketmage <tranremi@live.fr>
2024-12-29 17:46:56 +0100

    feature(audio): sound base

--- src/composables/useAudio.ts
+++ src/composables/useAudio.ts
@@ -1,17 +1,70 @@
-export const useAudio = () => {
-  const start = () => {
-    const audioCtx = new window.AudioContext()
+/** @pureIntonation @Limit5th */
+export const ji5LimitModesFreqRatios: Record<string, Record<string, number>> = {
+  NaturalMajor: {
+    Unison: 1,
+    MajorSecond: 9 / 8,
+    MajorThird: 5 / 4, //
+    PerfectFourth: 4 / 3,
+    PerfectFifth: 3 / 2,
+    MajorSixth: 5 / 3, //
+    MajorSeventh: 15 / 8, //
+    Octave: 2,
+  },
+  NaturalMinor: {
+    Unison: 1,
+    MajorSecond: 9 / 8,
+    MinorThird: 5 / 4, //
+    PerfectFourth: 4 / 3,
+    PerfectFifth: 3 / 2,
+    MinorSixth: 8 / 5, //
+    MinorSeventh: 16 / 9, //
+    Octave: 2,
+  },
+}
 
-    const oscillator = audioCtx.createOscillator()
-    const gainNode = audioCtx.createGain()
+/** @pureIntonation @Limit7th */
+export const ji7LimitModesFreqRatios: Record<string, Record<string, number>> = {
+  NaturalMajor: {
+    Unison: 1,
+    MajorSecond: 9 / 8,
+    MajorThird: 7 / 6, // CHANGED from 5/4
+    PerfectFourth: 4 / 3,
+    PerfectFifth: 3 / 2,
+    MajorSixth: 8 / 5, // CHANGED from 5/3 (to 8/5 ~or 7/4~)
+    // 'MajorSixth': 7/4, // CHANGED from 5/3 (to 8/5 ~or 7/4~)
+    MajorSeventh: 15 / 8, // CHANGED from 15/8 (to 15/8 ~or 9/5~)
+    // 'MajorSeventh': 9/5, // CHANGED from 15/8 (to 15/8 ~or 9/5~)
+    Octave: 2,
+  },
+  NaturalMinor: {
+    Unison: 1,
+    MajorSecond: 9 / 8,
+    MinorThird: 6 / 5, // CHANGED from 5/4 (~to 6/5~ or 7/6)
+    // 'MinorThird': 7/6, // CHANGED from 5/4 (~to 6/5~ or 7/6)
+    PerfectFourth: 4 / 3,
+    PerfectFifth: 3 / 2,
+    MinorSixth: 7 / 4, // CHANGED from 8/5
+    // 'MinorSeventh': 7/4, // CHANGED from 16/9 (to 16/9 or 7/4)
+    MinorSeventh: 16 / 9, // CHANGED from 16/9 (to 16/9 or 7/4)
+    Octave: 2,
+  },
+}
+export const useAudio = ({ frequency }: { frequency: number }) => {
+  const audioCtx = new window.AudioContext()
+  const oscillator = audioCtx.createOscillator()
+  const gainNode = audioCtx.createGain()
+  oscillator.connect(gainNode)
+  gainNode.gain.value = 0
+  gainNode.connect(audioCtx.destination)
 
-    oscillator.connect(gainNode)
-    gainNode.connect(audioCtx.destination)
-
+  const initialize = () => {
+    oscillator.frequency.value = frequency
     oscillator.start()
   }
 
   return {
-    start,
+    oscillator,
+    gainNode,
+    initialize,
   }
 }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,73 @@
 <script setup lang="ts">
-import { useAudio } from '@/composables/useAudio'
-const { start } = useAudio()
+import { ref, onMounted, onBeforeUnmount, type Ref } from 'vue';
+import { useAudio, ji5LimitModesFreqRatios, ji7LimitModesFreqRatios } from '@/composables/useAudio'
+
+const pristine = ref(true)
+const notes: Ref<string[]> = ref([])
+
+const C4 = 261.63;
+
+const { oscillator, gainNode, initialize } = useAudio({
+  frequency: C4
+})
+
+const eventKeys = "QWERTYUI".split("").map(_ => `Key${_}`);
+
+const { NaturalMajor } = ji7LimitModesFreqRatios
+const handleKeyDown = ({ code }: { code: string }) => {
+  const found = eventKeys.indexOf(code);
+  const toneKey = Object.keys(NaturalMajor)[found];
+  const factor = NaturalMajor[toneKey];
+
+  if (found > -1) {
+    gainNode.gain.value = 1
+    clearKey(code)
+    notes.value.push(code)
+    oscillator.frequency.value = C4 * factor;
+  }
+}
+const handleKeyUp = ({ code }: { code: string }) => {
+  console.log("keyup", code)
+
+  clearKey(code)
+
+  gainNode.gain.value = 0
+}
+
+const clearKey = (code: string) => {
+  const found = notes.value.indexOf(code)
+  if (found) {
+    notes.value.splice(found, 1);
+  }
+}
+
+const handleStart = () => {
+  pristine.value = false
+  initialize()
+}
+
+onMounted(() => {
+  document.addEventListener("keydown", handleKeyDown)
+  document.addEventListener("keyup", handleKeyUp)
+})
+
+onBeforeUnmount(() => {
+  oscillator.stop()
+  document.removeEventListener("keydown", handleKeyDown)
+  document.removeEventListener("keyup", handleKeyUp)
+});
+
 </script>
 
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <button @click="start">start</button>
+    <button @click="handleStart" v-if="pristine">start</button>
+    <div v-else class="keyboard">
+      <div :class="['key', { active: notes.indexOf(key) > -1 }]" v-for="key in eventKeys" :key="key">
+        {{ key }}
+      </div>
+      <div>{{ notes }}</div>
+    </div>
   </div>
 </template>
 
@@ -17,5 +78,20 @@ const { start } = useAudio()
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+button {
+  cursor: pointer;
+}
+
+.keyboard {
+  display: flex;
+  gap: 10px;
+}
+
+.key {
+  width: 120px;
+  height: 120px;
+  background-color: red;
 }
 </style>
